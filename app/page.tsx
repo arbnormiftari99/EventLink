789a324582f5696a876e12f22c817860aa0514e1
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-05-17 13:06:53 +0200

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <main>
+   <div className="h-screen flex items-center justify-center">
+    Hello from Eventslink
+    </div>
+  </main>
+  );
+}
