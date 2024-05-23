dc375eea41baf9d7654be8ee4d01d7f32f515d29
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-05-23 23:17:11 +0200

    Add clerkmiddleware, User actions with the CRUD function operations. Creating properties of user for the different operations

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 
 const isProtectedRoute = createRouteMatcher([
-  '/',
-  '/events/:id',
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
-  '/api/uploadthing'
+
 ]);
 
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-});
+}, {debug: true});
+
+
 
 export const config = {
   matcher: ["/((?!.*\..*|_next).*)", "/", "/(api|trpc)(.*)"],
