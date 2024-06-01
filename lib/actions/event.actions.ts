167dadaee33c1942d88b3abde450bdf889c341fc
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-06-01 22:57:34 +0200

    Function to get all events from the database, working further to fetch based on limit, page, category

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { CreateEventParams } from "@/types"
+import { CreateEventParams, GetAllEventsParams } from "@/types"
 import { connectDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
@@ -39,6 +39,29 @@ export const getEventById = async(eventId: any) => {
             throw new Error("Event not found");
         }
         return JSON.parse(JSON.stringify(event));
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export const getAllEvents= async({query, limit = 6, page, category}: GetAllEventsParams) => {
+
+    try {
+        await connectDatabase();
+        const conditions = {};
+        const eventsQuery = Event.find(conditions)
+        .sort({ createdAt: 'desc'})
+        .skip(0)
+        .limit(limit);
+        const events = await populateEvent(eventsQuery);
+        const eventsCount = await Event.countDocuments(conditions);
+        if(!events){
+            throw new Error("Event not found");
+        }
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit),
+        }
     } catch (error) {
         handleError(error);
     }
